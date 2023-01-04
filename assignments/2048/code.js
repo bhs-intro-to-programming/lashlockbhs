13be5b70d56f6a4a7a24507cefafab1ed958e5c5
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2023-01-04 15:31:19 -0800

    Updating assignments/2048/code.js

--- assignments/2048/code.js
+++ assignments/2048/code.js
@@ -108,18 +108,18 @@ let CoordsArray = []
 
 
 //key press detection
-registerOnclick((x, y) => {
-  drawFilledCircle(x, y, 1.7, 'white')
-  CoordsArray.push({ x, y })
-})
-
-registerOnKeyDown((Space) => {
-  ObjArray.push(new Shape(10, [vector(0, 0)], CoordsArray))
-  ObjArray[ObjArray.length - 1].drawShape()
-  drawFilledCircle(ObjArray[ObjArray.length - 1].centerX, ObjArray[ObjArray.length - 1].centerY, 2.5, "red")
-  CoordsArray = []
-})
-
+const createShape = () =>{
+  registerOnclick((x, y) => {
+    drawFilledCircle(x, y, 1.7, 'white')
+    CoordsArray.push({ x, y })
+  })
+  registerOnKeyDown((Space) => {
+    ObjArray.push(new Shape(10, [vector(0, 0)], CoordsArray))
+    ObjArray[ObjArray.length - 1].drawShape()
+    drawFilledCircle(ObjArray[ObjArray.length - 1].centerX, ObjArray[ObjArray.length - 1].centerY, 2.5, "red")
+    CoordsArray = []
+  })
+}
 
 class Shape {
   constructor(mass, actingForces, coordArray) {
