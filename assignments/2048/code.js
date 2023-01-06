867a53a64ca60464967bb05ab6969f30b229a873
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2023-01-05 21:45:57 -0800

    Updating assignments/2048/code.js

--- assignments/2048/code.js
+++ assignments/2048/code.js
@@ -112,12 +112,14 @@ const createShape = () =>{
   registerOnclick((x, y) => {
     drawFilledCircle(x, y, 1.7, 'white')
     CoordsArray.push({ x, y })
+    return
   })
   registerOnKeyDown((Space) => {
     ObjArray.push(new Shape(10, [vector(0, 0)], CoordsArray))
     ObjArray[ObjArray.length - 1].drawShape()
     drawFilledCircle(ObjArray[ObjArray.length - 1].centerX, ObjArray[ObjArray.length - 1].centerY, 2.5, "red")
     CoordsArray = []
+    return
   })
 }
 
