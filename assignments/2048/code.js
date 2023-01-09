5842c91e7ab3275afe05dff38b0ff6d5e327f765
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2023-01-08 18:08:39 -0800

    Updating assignments/2048/code.js

--- assignments/2048/code.js
+++ assignments/2048/code.js
@@ -199,5 +199,8 @@ const drawFrame = (time) => {
 
 animate(drawFrame)
 */
+
+const distance = (p1, p2) => Math.hypot(p1.x -p2.x, p1.y - p2.y)//math func
+
 const closePoints = (ar1, ar2) => ar1.filter(e => ar2.find(e2 => distance(e, e2)<=1) != undefined ? true : false)
 console.log(closePoints[{"x" : 10, "y" : 9}, {"x" : 10, "y" : 9}, {"x" : 10, "y" : 9}])
