8c84da129e6fbfe8668ba5bf078d9b61e4e777d6
lashlockbhs <111320483+lashlockbhs@users.noreply.github.com>
2023-01-08 18:04:11 -0800

    Updating assignments/2048/code.js

--- assignments/2048/code.js
+++ assignments/2048/code.js
@@ -200,4 +200,4 @@ const drawFrame = (time) => {
 animate(drawFrame)
 */
 const commonElements = (ar1, ar2) => ar1.filter(e => ar2.find(e2 => e2 == e) != undefined ? true : false)
-commonElements([1, 2, 3], [3, 4, 5])
+console.log(commonElements([1, 2, 3], [3, 4, 5]))
